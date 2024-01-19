c36c5d604d975040d90d04e5766eb7e0fd4c67f4
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-19 17:07:06 -0300

    Exclusão fisíca dos dados do médico

--- src/main/java/med/voll/api/domain/medico/Controller/MedicoController.java
+++ src/main/java/med/voll/api/domain/medico/Controller/MedicoController.java
@@ -13,13 +13,17 @@ import med.voll.api.domain.medico.dto.ListarMedicosDTO;
 import med.voll.api.domain.medico.entity.Medico;
 import med.voll.api.domain.medico.repository.MedicoRepository;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
@@ -68,6 +72,11 @@ public class MedicoController {
         var medico = medicoRepository.getReferenceById(atualizarDadosMedicoDTO.id());
         medico.atualizarMedicos(atualizarDadosMedicoDTO);
         return atualizarDadosMedicoDTO;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarMedico(@PathVariable UUID id){
+        medicoRepository.deleteById(id);
     }
 
 }
