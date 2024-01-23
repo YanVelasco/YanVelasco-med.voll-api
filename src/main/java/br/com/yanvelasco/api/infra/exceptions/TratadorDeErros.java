c9c6309f62dd95c95c42bafedc176e1e31123869
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-01-23 14:48:50 -0300

    Tratando erro 400

---
+++ src/main/java/br/com/yanvelasco/api/infra/exceptions/TratadorDeErros.java
@@ -0,0 +1,17 @@
+package br.com.yanvelasco.api.infra.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> error404(){
+        return ResponseEntity.notFound().build();
+    }
+
+}
